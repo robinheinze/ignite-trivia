1e732b578f24ab460755a9fc86d36186ad6f0021
Robin Heinze <robin.m.heinze@gmail.com>
2019-07-18 14:57:49 -0700

    Initial commit Ignite CLI version 3.1.3

---
+++ android/app/src/main/java/com/iceandfire/MainApplication.java
@@ -0,0 +1,55 @@
+package com.iceandfire;
+
+import android.app.Application;
+
+import com.facebook.react.ReactApplication;
+import org.devio.rn.splashscreen.SplashScreenReactPackage;
+import com.reactcommunity.rnlocalize.RNLocalizePackage;
+import com.oblador.keychain.KeychainPackage;
+import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
+import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
+import com.facebook.soloader.SoLoader;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainApplication extends Application implements ReactApplication {
+
+  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+    @Override
+    public boolean getUseDeveloperSupport() {
+      return BuildConfig.DEBUG;
+    }
+
+    @Override
+    protected List<ReactPackage> getPackages() {
+      return Arrays.<ReactPackage>asList(
+          new MainReactPackage(),
+            new SplashScreenReactPackage(),
+            new RNLocalizePackage(),
+            new KeychainPackage(),
+            new RNGestureHandlerPackage(),
+            new AsyncStoragePackage()
+      );
+    }
+
+    @Override
+    protected String getJSMainModuleName() {
+      return "index";
+    }
+  };
+
+  @Override
+  public ReactNativeHost getReactNativeHost() {
+    return mReactNativeHost;
+  }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    SoLoader.init(this, /* native exopackage */ false);
+  }
+}
---
+++ app/models/root-store/root-store.ts
@@ -0,0 +1,19 @@
+import { Instance, SnapshotOut, types } from "mobx-state-tree"
+import { NavigationStoreModel } from "../../navigation/navigation-store"
+
+/**
+ * An RootStore model.
+ */
+export const RootStoreModel = types.model("RootStore").props({
+  navigationStore: types.optional(NavigationStoreModel, {}),
+})
+
+/**
+ * The RootStore instance.
+ */
+export type RootStore = Instance<typeof RootStoreModel>
+
+/**
+ * The data of an RootStore.
+ */
+export type RootStoreSnapshot = SnapshotOut<typeof RootStoreModel>
